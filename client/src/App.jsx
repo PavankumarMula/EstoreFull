a12cb41f67b9ac03e559424afa5afd970c6ade72
PavankumarMula <pavanpappy7@gmail.com>
2026-05-20 23:42:29 +0530

    Implemented the product Detailed Page in FE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/Signin";
 import SignUpPage from "./pages/Signup";
+import ProductDetailPage from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </MainLayout>
   );
